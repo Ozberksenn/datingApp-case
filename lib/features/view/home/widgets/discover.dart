1fa43b2c69bd41e4de9410ad885abe25e5554c07
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2025-07-15 12:40:41 +0300

    updated

--- lib/features/view/home/widgets/discover.dart
+++ lib/features/view/home/widgets/discover.dart
@@ -1,13 +1,10 @@
 import 'package:datingapp/features/model/movie_model.dart';
 import 'package:datingapp/features/view-model/home/home_cubit.dart';
 import 'package:datingapp/features/widgets/loading.dart';
-import 'package:datingapp/product/extension/context_extension.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../../../product/constants/app_colors.dart';
-import '../../../widgets/padding.dart';
-import 'movie_info_tile.dart';
+import 'discover_card.dart';
 
 class Discover extends StatelessWidget {
   final List<MovieModel> movieList;
@@ -21,62 +18,13 @@ class Discover extends StatelessWidget {
           shrinkWrap: true,
           itemCount: movieList.length,
           itemBuilder: (context, index) {
-            return Container(
-              height: context.dynamicHeight(0.8),
-              decoration: conditionImage(cubit, movieList[index]),
-              child: Padding(
-                padding: ConstEdgeInsets.padding20(),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: InkWell(
-                        onTap:
-                            () => cubit.addFavourite(movieList[index], index),
-                        child:
-                            movieList[index].isFavorite == false
-                                ? Icon(
-                                  CupertinoIcons.heart,
-                                  color: AppColors.white,
-                                )
-                                : Icon(
-                                  CupertinoIcons.heart_fill,
-                                  color: AppColors.white,
-                                ),
-                      ),
-                    ),
-                    CustomSizedBox.paddingHeight(heightValue: 24.0),
-                    MovieInfoTile(movie: movieList[index]),
-                  ],
-                ),
-              ),
+            return DiscoverCard(
+              cubit: cubit,
+              movieList: movieList,
+              index: index,
             );
           },
         )
         : LoadingWidget();
-  }
-}
-
-BoxDecoration conditionImage(HomeCubit state, MovieModel movie) {
-  final imagePath = state.replaceImagePath(movie.poster!);
-  if (movie.poster != null && movie.poster != "") {
-    return BoxDecoration(
-      image: DecorationImage(fit: BoxFit.cover, image: NetworkImage(imagePath)),
-    );
-  } else if (movie.images != [] && movie.images != null) {
-    return BoxDecoration(
-      image: DecorationImage(
-        fit: BoxFit.cover,
-        image: NetworkImage(movie.images![0]),
-      ),
-    );
-  } else {
-    return BoxDecoration(
-      image: DecorationImage(
-        fit: BoxFit.cover,
-        image: AssetImage("assets/logo/SinFlixLogo.png"),
-      ),
-    );
   }
 }
---
+++ lib/features/view/home/widgets/discover_card.dart
@@ -0,0 +1,159 @@
+import 'dart:ui';
+
+import 'package:datingapp/features/widgets/radius.dart';
+import 'package:datingapp/product/extension/context_extension.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../../../../product/constants/app_colors.dart';
+import '../../../model/movie_model.dart';
+import '../../../view-model/home/home_cubit.dart';
+import '../../../widgets/padding.dart';
+import 'movie_info_tile.dart';
+
+class DiscoverCard extends StatelessWidget {
+  const DiscoverCard({
+    super.key,
+    required this.index,
+    required this.cubit,
+    required this.movieList,
+  });
+  final int index;
+  final HomeCubit cubit;
+  final List<MovieModel> movieList;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: context.dynamicHeight(0.8),
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(decoration: conditionImage(cubit, movieList[index])),
+          UnderBlackGradient(),
+          MovieCaption(cubit: cubit, movieList: movieList, index: index),
+        ],
+      ),
+    );
+  }
+}
+
+class MovieCaption extends StatelessWidget {
+  const MovieCaption({
+    super.key,
+    required this.cubit,
+    required this.movieList,
+    required this.index,
+  });
+
+  final HomeCubit cubit;
+  final List<MovieModel> movieList;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: ConstEdgeInsets.padding20(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Align(
+            alignment: Alignment.centerRight,
+            child: DiscoverCardHearth(
+              cubit: cubit,
+              movieList: movieList,
+              index: index,
+            ),
+          ),
+          CustomSizedBox.paddingHeight(heightValue: 24.0),
+          MovieInfoTile(movie: movieList[index]),
+        ],
+      ),
+    );
+  }
+}
+
+class UnderBlackGradient extends StatelessWidget {
+  const UnderBlackGradient({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 0,
+      left: 0,
+      right: 0,
+      height: 200,
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.transparent, AppColors.black],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DiscoverCardHearth extends StatelessWidget {
+  const DiscoverCardHearth({
+    super.key,
+    required this.cubit,
+    required this.movieList,
+    required this.index,
+  });
+
+  final HomeCubit cubit;
+  final List<MovieModel> movieList;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => cubit.addFavourite(movieList[index], index),
+      child: ClipRRect(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+          child: Container(
+            padding: ConstEdgeInsets.paddingSymetric(
+              horizontalPad: 12.0,
+              verticalPad: 26.0,
+            ),
+            decoration: BoxDecoration(
+              color: AppColors.grey.withValues(alpha: 0.5),
+              borderRadius: CustomRadius.radius28,
+              border: Border.all(width: 1, color: AppColors.lightGrey),
+            ),
+            child:
+                movieList[index].isFavorite == false
+                    ? Icon(CupertinoIcons.heart, color: AppColors.white)
+                    : Icon(CupertinoIcons.heart_fill, color: AppColors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+BoxDecoration conditionImage(HomeCubit state, MovieModel movie) {
+  final imagePath = state.replaceImagePath(movie.poster!);
+  if (movie.poster != null && movie.poster != "") {
+    return BoxDecoration(
+      image: DecorationImage(fit: BoxFit.cover, image: NetworkImage(imagePath)),
+    );
+  } else if (movie.images != [] && movie.images != null) {
+    return BoxDecoration(
+      image: DecorationImage(
+        fit: BoxFit.cover,
+        image: NetworkImage(movie.images![0]),
+      ),
+    );
+  } else {
+    return BoxDecoration(
+      image: DecorationImage(
+        fit: BoxFit.cover,
+        image: AssetImage("assets/logo/SinFlixLogo.png"),
+      ),
+    );
+  }
+}
